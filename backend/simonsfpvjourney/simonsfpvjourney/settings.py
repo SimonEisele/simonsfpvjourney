63307b2bcf65fc34e92aa2a6d0fb575d0a165f51
SimonEisele <simoneisele@icloud.com>
2026-04-18 17:12:15 +0200

    initial commit

--- backend/simonsfpvjourney/simonsfpvjourney/settings.py
+++ backend/simonsfpvjourney/simonsfpvjourney/settings.py
@@ -148,7 +148,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 

2c4d06923b4f4bff731e3f8d3c8d335dabd8d005
SimonEisele <simoneisele@icloud.com>
2026-04-19 12:25:35 +0200

    create superuser command

---
+++ backend/simonsfpvjourney/videos/management/commands/create_admin.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
+
+        if not username or not password:
+            self.stdout.write("Superuser not created (missing env vars)")
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write("Superuser already exists")
+            return
+
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        self.stdout.write("Superuser created successfully")
